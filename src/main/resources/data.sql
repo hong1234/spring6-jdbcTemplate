9c79948121a8e3fb98a43d3c2f6c078ed24926fc
hong le <vuanhde@yahoo.de>
2024-04-12 17:51:30 +0200

    2d.commit am 12.04.24

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,14 @@
 DROP TABLE IF EXISTS people;
 
 create table people (
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id int NOT NULL AUTO_INCREMENT,
   first_name varchar(20) NOT NULL,
   last_name varchar(20) NOT NULL,
   age int NOT NULL,
   gender enum ('Male', 'Female', 'Third') NOT NULL,
   created_on timestamp DEFAULT NULL,
-  updated_on timestamp DEFAULT NULL
+  updated_on timestamp DEFAULT NULL,
 --   created_on datetime DEFAULT NULL,
---   updated_on datetime DEFAULT NULL
+--   updated_on datetime DEFAULT NULL,
+  PRIMARY KEY (id)
 );
